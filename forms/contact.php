a7f342f065f0b375abddf6a58fb4225eb1af3333
Zeshawn Manzoor <zeshawnmanzoor@yahoo.com>
2023-02-14 18:18:12 -0500

    Updated php form
    Configured email with php without the email library

--- forms/contact.php
+++ forms/contact.php
@@ -1,3 +1,4 @@
+
 <?php
   /**
   * Requires the "PHP Email Form" library
@@ -6,6 +7,9 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
+
+
+  /*
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'zeshawnm.dev@gmail.com';
 
@@ -32,11 +36,73 @@
     'password' => secret',
     'port' => '25'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+  
+
+  // Replace contact@example.com with your real receiving email address
+  $to_email = 'zeshawnm.dev@gmail.com';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form fields and sanitize the input
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $subject = htmlspecialchars($_POST['subject']);
+  $message = htmlspecialchars($_POST['message']);
+
+  // Set up the email headers
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+  // Send the email
+  if (mail($to_email, $subject, $message, $headers)) {
+    echo "success";
+  } else {
+    echo "error";
+  }
+}
+*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = strip_tags(trim($_POST["name"]));
+  $name = str_replace(array("\r","\n"),array(" "," "),$name);
+  $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+  $subject = strip_tags(trim($_POST["subject"]));
+  $message = strip_tags(trim($_POST["message"]));
+  
+  // Set your email address where you want to receive emails
+  $to = "zeshawnm.dev@gmail.com";
+  
+  // Set the email subject
+  $subject = "New contact form submission: $subject";
+  
+  // Build the email content
+  $email_content = "Name: $name\n";
+  $email_content .= "Email: $email\n\n";
+  $email_content .= "Message:\n$message\n";
+  
+  // Build the email headers
+  $email_headers = "From: $name <$email>";
+  
+  // Send the email
+  if (mail($to, $subject, $email_content, $email_headers)) {
+    http_response_code(200);
+    echo "Thank You! Your message has been sent.";
+  } else {
+    http_response_code(500);
+    echo "Oops! Something went wrong and we couldn't send your message.";
+  }
+} else {
+  http_response_code(403);
+  echo "There was a problem with your submission, please try again.";
+}
 ?>
+
+
